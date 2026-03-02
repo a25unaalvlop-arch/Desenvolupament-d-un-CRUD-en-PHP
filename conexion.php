1492fcd73fedf9344bac4629260b9ef68f775ae5
Unai Alvarez <a25unaalvlop@inspedralbes.cat>
2026-03-02 14:23:31 +0100

    Funcionant 1

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$usuario = "root";
-$contrasenia = "";
-$base_de_datos = "videojuegos";
+$usuario = "a25unaalvlop_";
+$contrasenia = "hbBoXRa5NvVO1sWB";
+$base_de_datos = "a25unaalvlop_videojuegos";
 $mysqli = new mysqli($host, $usuario, $contrasenia, $base_de_datos);
 if ($mysqli->connect_errno) {
     echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
---
+++ registrar.php
@@ -0,0 +1,12 @@
+<?php include_once "header.php"; ?>
+<?php
+$mysqli = include_once "conexion.php";
+$nombre = $_POST["nombre"];
+$descripcion = $_POST["descripcion"];
+$sentencia = $mysqli->prepare("INSERT INTO videojuegos
+(nombre, descripcion)
+VALUES
+(?, ?)");
+$sentencia->bind_param("ss", $nombre, $descripcion);
+$sentencia->execute();
+header("Location: listar.php");
